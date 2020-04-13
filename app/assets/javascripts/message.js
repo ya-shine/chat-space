2b0b2fdc7d88baa2c05071504dcf59331897bf38
ya-shine <shunsoku.no.10@gmail.com>
2020-04-13 17:30:00 +0900

    implement asynchronous communication for message sending function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,67 @@
+$(function(){
+
+  function buildHTML(message){
+    if ( message.image) {
+      var html =
+        `<div class="message__content">
+          <div class="message__content__info">
+            <div class="message__content__info--memmber">
+              ${message.user_name}
+            </div>
+            <div class="message__content__info--date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__content__message">
+            <p class="message__content__message--text">
+              ${message.body}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message__content">
+          <div class="message__content__info">
+            <div class="message__content__info--memmber">
+              ${message.user_name}
+            </div>
+            <div class="message__content__info--date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__content__message">
+            <p class="message__content__message--text">
+              ${message.body}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url =$(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('form')[0].reset();
+      $('.form__submit').prop('disabled', false);
+    })
+    .fail(function(){
+      alert("メッセージ送信に失敗しました");
+    });
+  })
+});
